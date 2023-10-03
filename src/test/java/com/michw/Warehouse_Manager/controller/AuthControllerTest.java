076506dd1674f536cd69dd6393f1119f831afb65
mich-wierzch <theordinarymaiky@gmail.com>
2023-10-03 19:44:24 +0200

    Updated AuthController test

--- src/test/java/com/michw/Warehouse_Manager/controller/AuthControllerTest.java
+++ src/test/java/com/michw/Warehouse_Manager/controller/AuthControllerTest.java
@@ -1,19 +1,22 @@
 package com.michw.Warehouse_Manager.controller;
 
+
 import com.michw.Warehouse_Manager.dto.AuthRequest;
-import com.michw.Warehouse_Manager.dto.AuthResponse;
 import com.michw.Warehouse_Manager.dto.RegisterRequest;
 import com.michw.Warehouse_Manager.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 
 public class AuthControllerTest {
 
@@ -31,32 +34,53 @@ public class AuthControllerTest {
     @Test
     public void shouldReturnTokenAsAResponseToSuccessfulRegistration(){
         //given
-        RegisterRequest registerRequest = new RegisterRequest();
-        AuthResponse authResponse = new AuthResponse("tokenPlaceholder");
-        //when
-        when(authService.register(registerRequest)).thenReturn(authResponse);
-        ResponseEntity<AuthResponse> responseEntity = authController.register(registerRequest);
-        //then
-        assert responseEntity.getStatusCode() == HttpStatus.OK;
-        assert responseEntity.getBody() != null;
-        assert responseEntity.getBody().getToken().equals(authResponse.getToken());
+        RegisterRequest registerRequest = new RegisterRequest("Testttt", "Userrr", "TestUser123@gmail.com", "TesAdat123123@@");
+        String expectedToken = generateRandomJwtToken();
 
-        Mockito.verify(authService, times(1)).register(registerRequest);
+        //when
+        when(authService.register(registerRequest)).thenReturn(new ResponseEntity<>(expectedToken, HttpStatus.OK));
+        var response = authController.register(registerRequest);
+
+        //then
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(expectedToken, response.getBody());
+
+        verify(authService, times(1)).register(registerRequest);
     }
+
     @Test
-    public void shouldReturnTokenAsAResponseToSuccessfulLogin(){
+    public void shouldAuthenticateAndReturnToken(){
         //given
-        AuthRequest authRequest = new AuthRequest();
-        AuthResponse authResponse = new AuthResponse("tokenPlaceholder");
-        //when
-        when(authService.authenticate(authRequest)).thenReturn(authResponse);
-        ResponseEntity<AuthResponse> responseEntity = authController.authenticate(authRequest);
-        //then
-        assert responseEntity.getStatusCode() == HttpStatus.OK;
-        assert responseEntity.getBody() != null;
-        assert responseEntity.getBody().getToken().equals(authResponse.getToken());
+        RegisterRequest registerRequest = new RegisterRequest("Testttt", "Userrr", "TestUser123@gmail.com", "TesAdat123123@@");
+        authService.register(registerRequest);
+        AuthRequest authRequest = new AuthRequest(
+                registerRequest.getEmail(),
+                registerRequest.getPassword()
+        );
+        String jwtToken = generateRandomJwtToken();
 
-        Mockito.verify(authService, times(1)).authenticate(authRequest);
+        //when
+        when(authService.authenticate(authRequest)).thenReturn(new ResponseEntity<>(jwtToken, HttpStatus.OK));
+        var response = authController.authenticate(authRequest);
+
+        //then
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(jwtToken, response.getBody());
+
+        verify(authService, times(1)).authenticate(authRequest);
+
+
+
+
     }
+
+    private String generateRandomJwtToken(){
+        return Jwts.builder()
+                .setSubject("TestUser@gmail.com")
+                .claim("roles", "user")
+                .signWith(SignatureAlgorithm.HS256, "14b8b0acee3bd31cd8d3bb141cd6f1921fec7adfa77e28164f0ab8db172eada8")
+                .compact();
+    }
+
 
 }
